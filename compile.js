c163a6e1e5ab01f081b73eb3444fd3a8fa231ddb
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2021-07-31 11:22:01 +0530

    Basic Folder structure

---
+++ compile.js
@@ -0,0 +1,14 @@
+// Importing prerequisite files
+const solc = require("solc");
+const path = require("path");
+const fs = require("fs");
+
+// Path to Inbox folder
+const inboxPath = path.resolve(__dirname, "Contracts", "inbox.sol");
+
+// Reading contents of Solidity file
+const sourceFile = fs.readFileSync(inboxPath, "utf8");
+
+// Compiling the solidity contract
+var x = solc.compile(sourceFile, 1);
+console.log(x);
